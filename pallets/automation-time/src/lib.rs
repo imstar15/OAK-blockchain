0a63f61e2f89979ed1408fd8eda3d1e7e82049c4
imstar15 <imstar15@qq.com>
2022-06-30 13:30:00 +0800

    Fix calculation inaccuracy in is_valid_time function.

--- pallets/automation-time/src/lib.rs
+++ pallets/automation-time/src/lib.rs
@@ -502,7 +502,7 @@ pub mod pallet {
 				return Ok(())
 			}
 
-			let remainder = scheduled_time % 3600;
+			let remainder = scheduled_time.checked_rem(3600).ok_or(ArithmeticError::Overflow)?;
 			if remainder != 0 {
 				Err(<Error<T>>::InvalidTime)?;
 			}
@@ -511,8 +511,9 @@ pub mod pallet {
 			if scheduled_time <= current_time_slot {
 				Err(<Error<T>>::PastTime)?;
 			}
-
-			if scheduled_time > current_time_slot + T::MaxScheduleSeconds::get() {
+			
+			let max_schdule_time = current_time_slot.checked_add(T::MaxScheduleSeconds::get()).ok_or(ArithmeticError::Overflow)?;
+			if scheduled_time > max_schdule_time {
 				Err(Error::<T>::TimeTooFarOut)?;
 			}
 
