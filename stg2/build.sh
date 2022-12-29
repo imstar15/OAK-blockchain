c1fc3f77aaaa2c2d1bce4fb7242210027e4981f1
imstar15 <imstar15@qq.com>
2022-12-29 22:04:37 +0800

    Update chain spec

--- stg2/build.sh
+++ stg2/build.sh
@@ -1,5 +1,3 @@
 cargo build --release --features turing-node
-./target/release/oak-collator export-genesis-state --chain=turing-dev > genesis-state
-./target/release/oak-collator export-genesis-wasm --chain=turing-dev > genesis-wasm
 
-./target/release/oak-collator build-spec --disable-default-bootnode --chain turing-dev > stg2/chain-spec-chain.json
+./target/release/oak-collator build-spec --disable-default-bootnode --chain turing-dev > stg2/chain-spec-plain.json
--- stg2/gen.sh
+++ stg2/gen.sh
@@ -1,3 +1,3 @@
-./target/release/oak-collator build-spec --disable-default-bootnode --raw --chain=stg2/chain-spec-chain.json > stg2/chain-spec.json
+./target/release/oak-collator build-spec --disable-default-bootnode --raw --chain=stg2/chain-spec-plain.json > stg2/chain-spec.json
 ./target/release/oak-collator export-genesis-state --chain=stg2/chain-spec.json > stg2/genesis-state
 ./target/release/oak-collator export-genesis-wasm --chain=stg2/chain-spec.json > stg2/genesis-wasm
