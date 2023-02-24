3114a3667b84e1b7a49e0351c71eabe9a50c31b1
imstar15 <imstar15@qq.com>
2023-02-25 00:13:30 +0800

    type XcmReserveTransferFilter = Everything;

--- runtime/turing/src/xcm_config.rs
+++ runtime/turing/src/xcm_config.rs
@@ -313,7 +313,7 @@ impl pallet_xcm::Config for Runtime {
 	// Needs to be `Everything` for local testing.
 	type XcmExecutor = XcmExecutor<XcmConfig>;
 	type XcmTeleportFilter = Everything;
-	type XcmReserveTransferFilter = Nothing;
+	type XcmReserveTransferFilter = Everything;
 	type Weigher = FixedWeightBounds<UnitWeightCost, Call, MaxInstructions>;
 	type LocationInverter = LocationInverter<Ancestry>;
 	type Origin = Origin;
